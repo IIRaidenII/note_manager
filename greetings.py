b4ef6bc738dd89e2940da88710280047e535d96c
IIRaidenII <maksim-borisov-08@mail.ru>
2025-01-16 21:15:44 +0800

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Raiden"
+title = "Lesson sign-off"
+content = "Class times and days"
+status = "active"
+created_date = "16-06-2024"
+issue_date = "16-06-2025"
+
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Описание:", content)
+print("Статус:", status)
+print("Дата создания:", created_date)
+print("ДАта истечения:", issue_date)
