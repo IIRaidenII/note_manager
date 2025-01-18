737304539b4924bba10b37022617ea46e22d16d9
IIRaidenII <maksim-borisov-08@mail.ru>
2025-01-18 18:54:37 +0800

    final.py

--- final.py
+++ final.py
@@ -1,13 +1,13 @@
 information = {
-"Name":str(input("Введите своё имя: ")),
-"Content":str(input("Введите содержание заметки: ")),
-"Status":str(input("Введите статус заметки: ")),
-"Created_date":str(input("Введите дату создания: ")),
-"Issue_date":str(input("Введите дату истечения: "))
+"Name":"Raiden",
+"Content":"Расписание занятий",
+"Status":"Активно",
+"Created_date": "16.06.2024",
+"Issue_date":"16.06.2025",
 }
-tips = {
-"tip1":str(input("Введите первую заметку: ")),
-"tip2":str(input("Введите вторую заметку: "))
+titles = {
+"title1":"Учеба",
+"title2":"Уроки"
 }
 
 print("Ваше имя:", information["Name"])
@@ -15,5 +15,4 @@ print("Содержание заметки:", information["Content"])
 print("Статус:", information["Status"])
 print("Дата создания:", information["Created_date"])
 print("Дата изменения:", information["Issue_date"])
-print("Первый заголовок:", tips["tip1"], "; Второй заголовок:", tips["tip2"])
-
+print("Первый заголовок:", titles["title1"], "; Второй заголовок:", titles["title2"])
