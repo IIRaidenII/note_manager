e514863a0527f908ac62dae7307800e179a5ff99
IIRaidenII <maksim-borisov-08@mail.ru>
2025-01-18 18:52:52 +0800

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -2,8 +2,8 @@ question = input("Вы действительно хотите сменить д
 if question == "Нет":
     print("Смена даты и истечения заметки отменена ")
 if question == "Да":
-    created_date = str(input("Введите новую дату создания (День, месяц, год; через .) "))
-    issue_date = str(input("Введите новую дату истечения (День, месяц, год; через .) "))
+    created_date = str(input("Введите новую дату создания (День, месяц, год; через .): "))
+    issue_date = str(input("Введите новую дату истечения (День, месяц, год; через .): "))
     created_date1 = created_date[0:5]
     issue_date1 = issue_date[0:5]
     print("Готово! Теперь, ваша заметка создана:", created_date1, ", а дата истечения:", issue_date1)
