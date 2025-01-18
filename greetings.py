2a1f10dcace2d2ebf9861c946bad233ac3d6b915
IIRaidenII <maksim-borisov-08@mail.ru>
2025-01-18 18:52:04 +0800

    greetings.py

--- greetings.py
+++ greetings.py
@@ -1,13 +1,13 @@
 username = "Raiden"
-title = "Lesson sign-off"
-content = "Class times and days"
-status = "active"
-created_date = "16-06-2024"
-issue_date = "16-06-2025"
+title = "Учеба"
+content = "Расписание занятий"
+status = "Активно"
+created_date = "16.06.2024"
+issue_date = "16.06.2025"
 
 print("Имя пользователя:", username)
 print("Заголовок:", title)
 print("Описание:", content)
 print("Статус:", status)
 print("Дата создания:", created_date)
-print("ДАта истечения:", issue_date)
+print("Дата истечения:", issue_date)
