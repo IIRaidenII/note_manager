04edaaba6a27457d777f507a2ff2896227b73c79
IIRaidenII <maksim-borisov-08@mail.ru>
2025-01-18 17:38:02 +0800

    add_list.py

--- add_list.py
+++ add_list.py
@@ -2,4 +2,6 @@ tip1 = input("Введите вашу первую заметку: ")
 tip2 = input("Введите вашу вторую заметку: ")
 tip3 = input("Введите вашу третью заметку: ")
 all_tips = [tip1, tip2, tip3,]
-print(all_tips)
+print("1.", all_tips[0])
+print("2.", all_tips[1])
+print("3.", all_tips[2])
